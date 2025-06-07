bd794100209e73c7041ce6d237eb50c5e4b0e99e
kbib0002 <kyleedb@icloud.com>
2025-06-07 17:50:16 +1000

    Ran build successfully

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Al0clC0LxB6XbPLGAJX1DaHPvOY79EvdvZtu7180Dq8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
